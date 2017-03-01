b565e37f8af1a453573945d52c4bf7071c435950
sreekar <sreekarvsp@gmail.com>
2017-03-01 10:54:05 -0800

    Initial commit

---
+++ LeetCode_2/src/amazon/SetMatrixZero.java
@@ -0,0 +1,81 @@
+package amazon;
+
+public class SetMatrixZero {
+
+	public static void main(String[] args){
+		int[][] matrix = { {1,2,3,4},
+		           		   {5,6,0,8},
+		                   {9,10,11,12},
+		                   {13,14,15,16}
+                         };
+
+       matrixZero(matrix);
+	}
+
+	private static void matrixZero(int[][] matrix) {
+		int m = matrix.length;
+		int n = matrix[0].length;
+			
+
+		boolean rowHasZero = false;
+		boolean columnHasZero = false;
+		
+		for(int i=0; i<n; i++){
+            if(matrix[0][i]==0){
+                rowHasZero = true;
+                break;
+            }
+        }
+		
+		for(int i=0; i<m; i++){
+            if(matrix[i][0]==0){
+            	columnHasZero = true;
+                break;
+            }
+        }
+		
+		 for(int i=1; i<m; i++){
+	            for(int j=1; j<n; j++){
+	                if(matrix[i][j]==0){
+	                    matrix[i][0] = 0;
+	                    matrix[0][j] = 0;
+	                }
+	            }
+	        }
+		 
+		 
+		 for(int j=1;j<n; j++){
+	            if(matrix[0][j]==0){
+	                nullifyCol(matrix, j, m, n);
+	            }
+	        }
+		 
+		 
+		 for(int i=1; i<m; i++){
+	            if(matrix[i][0]==0){
+	                nullifyRow(matrix, i, m, n);
+	            }
+	        }
+	        
+	        if(rowHasZero){
+	            nullifyRow(matrix, 0, m, n);
+	        }
+	        if(columnHasZero){
+	            nullifyCol(matrix, 0, m, n);
+	        }
+		 
+	}
+
+	private static void nullifyCol(int[][] matrix, int j, int m, int n) {
+		 for(int row=0; row<m; row++){
+	            matrix[row][j] = 0;
+	        }
+		
+	}
+	
+	public static void nullifyRow(int[][] matrix, int i, int m, int n){
+        for(int col=0; col<n; col++){
+            matrix[i][col] = 0;
+        }
+    }
+}
